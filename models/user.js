1f08bade9e0975eb5795157e785a22997fe0ddf4
Aditya Gaur <aditya.gaur01@nagarro.com>
2019-10-17 10:06:23 +0530

    second commit

---
+++ models/user.js
@@ -0,0 +1,20 @@
+/*
+Author: - Aditya Raj Gaur
+*/
+module.exports = (sequelize, type) => {
+    return sequelize.define('users', {
+        user_id: {
+          type: type.INTEGER,
+          primaryKey: true,
+          autoIncrement: true
+        },
+        username: {
+            type: type.STRING,
+            defaultValue: null
+        },
+        upassword:{
+            type: type.STRING,
+            dafaultValue: null
+        }
+    })
+} 
---
+++ myConfig.js
@@ -0,0 +1,12 @@
+/*
+Author: - Aditya Raj Gaur
+*/
+const express = require('express');
+module.exports = {
+    mysql: require('mysql'),
+    exphbs: require('express-handlebars'),
+    bodyparser: require('body-parser'),
+    session: require('express-session'),
+    app: express(),
+    express: express
+};
